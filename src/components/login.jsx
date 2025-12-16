b7a9e15e963f97da737e1c5f89c92286950306ae
Macaron33-T <tarm3521@gmail.com>
2025-12-16 14:28:27 +0700

    Add tailwind

---
+++ src/components/login.jsx
@@ -0,0 +1,15 @@
+// import React from "react";
+
+const login = () => {
+  return (
+    <div>
+      <div>
+        <h1>Login</h1>
+        <input type="email" />
+        <label htmlFor=""></label>
+      </div>
+    </div>
+  );
+};
+
+export default login;
