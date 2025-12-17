aa42854d8d8067d9a3bf30913709d745f76445d2
JackJack2405 <chutikankin7442@gmail.com>
2025-12-17 16:34:38 +0700

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,68 @@
+import { Link } from "react-router-dom";
+import { BsCart4 } from "react-icons/bs";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-[#A6EAFF] shadow-sm">
+      <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+
+        {/* LEFT : Logo */}
+        <Link
+          to="/"
+          className="flex items-center gap-2 font-['Jua'] text-xl"
+        >
+          <img
+          src="/logo.png"
+          alt="LovinCandy logo"
+          className="h-10 w-auto object-contain md:h-14"
+          />
+        <span>LovinCandy</span>
+        </Link>
+
+
+        {/* CENTER : Menu */}
+        <ul className="hidden md:flex gap-10 font-['Jua'] text-lg">
+          <li>
+            <Link to="/" className="hover:opacity-70">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/customize" className="hover:opacity-70">
+              Customize
+            </Link>
+          </li>
+          <li>
+            <Link to="/product" className="hover:opacity-70">
+              Product
+            </Link>
+          </li>
+          <li>
+            <Link to="/profile" >My Profile</Link>
+          </li>
+        </ul>
+
+        {/* RIGHT : Cart + Sign in/Register */}
+        <div className="flex items-center gap-4">
+
+          {/* Cart */}
+          <Link
+            to="/shoppingcart"
+            className="text-xl hover:scale-110 transition"
+            aria-label="Shopping Cart"
+          >
+            <BsCart4 className="text-2xl"/>
+          </Link>
+
+          {/* Sign in | Register (หน้าเดียวกัน) */}
+          <Link
+            to="/auth"
+            className="bg-white px-4 py-1 rounded-full text-sm shadow hover:bg-gray-100 transition"
+          >
+            Sign in | Register
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
