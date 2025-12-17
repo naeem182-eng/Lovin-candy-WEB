8350662443c2b49756bad227b3b6ddbcdb50147c
JackJack2405 <chutikankin7442@gmail.com>
2025-12-17 16:05:57 +0700

    add-2

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,14 @@
-function ProductCard() {
-    return (
-        <div></div>
-    )
+export default function ProductCard({ image, name, price }) {
+  return (
+    <div className="bg-pink-100 rounded-3xl p-4 flex flex-col items-center gap-3">
+      <img src={image} alt={name} className="w-32 h-32 object-contain" />
+
+      <p className="text-pink-600 font-bold text-center">{name}</p>
+      <p className="text-sm text-gray-600">{price} ฿</p>
+
+      <button className="px-4 py-1 rounded-xl border border-pink-400 text-pink-500 hover:bg-pink-200">
+        I want this
+      </button>
+    </div>
+  );
 }
--- src/views/Products.jsx
+++ src/views/Products.jsx
@@ -1,10 +1,38 @@
-import Navbar from "../components/NavBar";
+
+
+const mockProducts = [
+  {
+    id: 1,
+    name: "Cotton Candy Lollipop Bag",
+    price: 199,
+    image: "/lollipop1.png",
+  },
+  {
+    id: 2,
+    name: "Orange Creamsicle Swirl",
+    price: 159,
+    image: "/lollipop2.png",
+  },
+  {
+    id: 3,
+    name: "Orange Cream Lollipop Bag",
+    price: 179,
+    image: "/lollipop3.png",
+  },
+];
 
 export default function Products() {
-    return (
-        <div>
-            <Navbar />
-        </div>
-    )
+  return (
+    <section>
+      <h1 className="text-3xl font-bold text-center bg-sky-200 py-6 rounded-full mb-10">
+        Product
+      </h1>
 
+      <div className="grid grid-cols-4 gap-8">
+        {mockProducts.map((item) => (
+          <ProductCard key={item.id} {...item} />
+        ))}
+      </div>
+    </section>
+  );
 }
