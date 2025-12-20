84a74dea0497f8312209de9139b20eb47141a98e
Macaron33-T <tarm3521@gmail.com>
2025-12-20 19:41:15 +0700

    Fixed login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,12 +1,16 @@
 // import React from "react";
+import { Link } from "react-router-dom";
+import { BiUser } from "react-icons/bi";
+import { AiOutlineUnlock } from "react-icons/ai";
 
 const login = () => {
   return (
     <div>
       <div className="bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative">
-        <h1 className="text-4xl text-white font-blod text-center mb-6">
+        <h1 className="text-4xl text-white font-bold text-center mb-6">
           Login
         </h1>
+
         <form action="">
           <div className="relative my-4">
             <input
@@ -16,11 +20,13 @@ const login = () => {
             />
             <label
               htmlFor=""
-              className="abosolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:txt-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              className="absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Your Email
             </label>
+            <BiUser className="absolute top-4 right-4" />
           </div>
+
           <div className="relative my-4">
             <input
               type="password"
@@ -29,25 +35,29 @@ const login = () => {
             />
             <label
               htmlFor=""
-              className="abosolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:txt-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              className="absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Your Password
             </label>
+            <AiOutlineUnlock className="absolute top-4 right-4" />
           </div>
+
           <div>
             <div>
-              <inpit type="checkbox" name="" id="" />
-              <label htmlFor="Remember Me"></label>
+              <input type="checkbox" name="" id="" />
+              <label htmlFor="Remember Me">Remember Me</label>
             </div>
             <span>Forgot Password?</span>
           </div>
+
           <button className="" type="submit">
             Login
           </button>
+
           <div>
-            <spn>
+            <span>
               New Here? <Link to="Register">Create an Account</Link>
-            </spn>
+            </span>
           </div>
         </form>
       </div>
